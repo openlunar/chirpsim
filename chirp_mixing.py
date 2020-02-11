f08b7c2bfe9995fe31c593cd3d20d2b4be0cd673
Aaron Goldstein <aaron@openlunar.org>
2020-02-10 18:40:14 -0800

    Using interpolation to do doppler shifting instead

--- chirp_mixing.py
+++ chirp_mixing.py
@@ -13,16 +13,17 @@ f_rate = 150
 # lo characteristics
 lo_f = 100
 
-size = 10000
+size = 20000
 
 x = np.linspace(0.0, t_max, size)
 shifted_x = np.zeros(size)
 shifted_lo = np.zeros(size)
 
 signal = np.zeros(size)
-lo = np.zeros(size)
+encode_lo = np.zeros(size)
 output = np.zeros(size)
 
+decode_lo = np.zeros(size)
 decoded = np.zeros(size)
 
 delta_t = t_max / size
@@ -30,41 +31,43 @@ delta_t = t_max / size
 f_scan = 0
 angle = 0
 
-doppler = 0.00005
+doppler = 0.001
 
 for index, t in np.ndenumerate(x):
 
     f = f_min + f_scan % (f_max - f_min)
 
     signal[index] = np.sin(angle)
-    lo[index] = np.sin(t*2*np.pi*lo_f)
-    output[index] = signal[index] * lo[index]
+    encode_lo[index] = np.sin(t*2*np.pi*lo_f)
+    output[index] = signal[index] * encode_lo[index]
 
-    shifted_x[index] = t * (1-doppler)
+    shifted_x[index] = t * (1+doppler)
 
     angle = angle + delta_t*2*np.pi*f
     f_scan = (f_scan + delta_t*f_rate)
 
-for index, t in np.ndenumerate(shifted_x):
+shifted_output = np.interp(shifted_x, x, output)
+
+for index, t in np.ndenumerate(x):
     
-    shifted_lo[index] = np.sin(t*2*np.pi*lo_f)
-    decoded[index] = output[index] / shifted_lo[index] if shifted_lo[index] else 0
+    decode_lo[index] = np.sin(t*2*np.pi*lo_f*(1+doppler))
+    decoded[index] = shifted_output[index] / decode_lo[index] if decode_lo[index] else 0
 
 
 ax1 = plt.subplot(511)
 plt.plot(x, signal)
 
 ax2 = plt.subplot(512, sharex=ax1)
-plt.plot(x, lo)
+plt.plot(x, encode_lo)
 
 ax3 = plt.subplot(513, sharex=ax2)
 plt.plot(x, output)
 
 ax4 = plt.subplot(514, sharex=ax3)
-plt.plot(shifted_x, output)
+plt.plot(x, shifted_output)
 
 ax5 = plt.subplot(515, sharex=ax4)
-plt.plot(shifted_x, decoded)
+plt.plot(x, decoded)
 
 plt.ylim(-1,1)
 
