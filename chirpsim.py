546fc821a08c6ba3b74b0374c7fa3947ba755e80
Aaron Goldstein <aaronmgoldstein@gmail.com>
2020-02-12 16:35:32 -0800

    Add ffts and make colors more distinct

--- chirpsim.py
+++ chirpsim.py
@@ -11,9 +11,9 @@ f_max = f_center + 0.5 * bw
 f_rate = 150
 
 # lo characteristics
-lo_f = 100
+lo_f = 150
 
-size = 20000
+size = 30000
 
 x = np.linspace(0.0, t_max, size)
 shifted_x = np.zeros(size)
@@ -32,7 +32,7 @@ delta_t = t_max / size
 f_scan = 0
 angle = 0
 
-doppler = 0.01
+doppler = 0.1
 
 for index, t in np.ndenumerate(x):
 
@@ -55,6 +55,28 @@ for index, t in np.ndenumerate(x):
     unmixed[index] = shifted_output[index] / decode_lo[index] if decode_lo[index] else 0
     unmixed_uncorrected[index] = shifted_output[index] / encode_lo[index] if encode_lo[index] else 0
 
+# fft setup and plotting
+fft_size = size*10
+Y_signal = np.abs(np.fft.rfft(signal, n=fft_size))
+Y_encode_lo = np.abs(np.fft.rfft(encode_lo, n=fft_size))
+Y_output = np.abs(np.fft.rfft(output, n=fft_size))
+Y_shifted_output = np.abs(np.fft.rfft(shifted_output, n=fft_size))
+Y_unmixed = np.abs(np.fft.rfft(unmixed, n=fft_size))
+f = np.fft.rfftfreq(fft_size, delta_t)
+
+plt.plot(f, Y_signal)
+plt.xscale('log')
+plt.plot(f, Y_encode_lo, 'tab:orange')
+plt.xscale('log')
+plt.plot(f, Y_output, 'tab:green')
+plt.xscale('log')
+plt.plot(f, Y_shifted_output, 'tab:red')
+plt.xscale('log')
+plt.plot(f, Y_unmixed, 'tab:cyan')
+plt.xscale('log')
+plt.xlim(10, 300)
+
+# time domain plotting
 fig, axs = plt.subplots(6, 1)
 axs[0].plot(x, signal)
 axs[0].set_title('Chirp ' + str(f_center) + 'hz center, ' + str(bw) + 'hz BW')
@@ -64,9 +86,9 @@ axs[2].plot(x, output, 'tab:green')
 axs[2].set_title('Mixed Signal')
 axs[3].plot(x, shifted_output, 'tab:red')
 axs[3].set_title('Mixed Signal w/ ' + str(doppler*100) + '% Doppler Shift')
-axs[4].plot(x, unmixed, 'tab:blue')
+axs[4].plot(x, unmixed, 'tab:cyan')
 axs[4].set_title('De-Mixed Signal w/ Shifted LO')
-axs[5].plot(x, unmixed_uncorrected, 'tab:blue')
+axs[5].plot(x, unmixed_uncorrected, 'tab:cyan')
 axs[5].set_title('De-Mixed Signal w/ Original LO')
 
 for ax in axs.flat:
